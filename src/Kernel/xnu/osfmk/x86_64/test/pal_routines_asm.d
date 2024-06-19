4c053b95e2b3451bab140dae4923eb57ff8922ba
LEE <15267030696@163.com>
2024-06-19 11:58:28 +0800

    1.更新文档； 2.新增 test 文件夹；

---
+++ src/Kernel/xnu/osfmk/x86_64/test/pal_routines_asm.d
@@ -0,0 +1,25 @@
+start.o: start.s \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/asm.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/proc_reg.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/postcode.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/assym.s \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/cpuid.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/EXPORT_HDRS/bsd/sys/appleapiopts.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/acpi.h
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/asm.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/proc_reg.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/postcode.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/assym.s:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/cpuid.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/EXPORT_HDRS/bsd/sys/appleapiopts.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/acpi.h:
---
+++ src/Kernel/xnu/osfmk/x86_64/test/start.d
@@ -0,0 +1,144 @@
+start.o: start.s meta_features.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/hibernation.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/crypto.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/encrypted_swap.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/kdebug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_assert.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_cluster_stats.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_counters.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_flipc.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ipc_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ipc_test.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_kdp.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_serial_kdp.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_kprof.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ldebug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_mp_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_pagemap.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_vm_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_page_hash_stats.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mig_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/vm_cpm.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/stack_usage.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/importance_inheritance.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/importance_debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_ecc_logging.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_dtrace.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/no_kextd.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_quiesce_counter.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_clutch.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sched_proto.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sched_grrr_core.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/development.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/debug.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_xnupost.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sysdiagnose.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_io_compression_stats.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_phantom_cache.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/monotonic.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/fb.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/profile.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/asm.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/proc_reg.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/postcode.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/assym.s \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/cpuid.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/EXPORT_HDRS/bsd/sys/appleapiopts.h \
+  /Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/acpi.h
+
+meta_features.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/hibernation.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/crypto.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/encrypted_swap.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/kdebug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_assert.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_cluster_stats.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_counters.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_flipc.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ipc_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ipc_test.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_kdp.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_serial_kdp.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_kprof.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_ldebug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_mp_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_pagemap.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_vm_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mach_page_hash_stats.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/mig_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/vm_cpm.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/stack_usage.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/importance_inheritance.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/importance_debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_ecc_logging.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_dtrace.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/no_kextd.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_quiesce_counter.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_clutch.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sched_proto.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sched_grrr_core.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/development.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/debug.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_xnupost.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_sysdiagnose.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_io_compression_stats.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/config_phantom_cache.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/monotonic.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/fb.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/profile.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/asm.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/proc_reg.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/postcode.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/RELEASE_X86_64/osfmk/RELEASE/assym.s:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/cpuid.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/build/src/Kernel/xnu/xnu_build/src/xnu-build/EXPORT_HDRS/bsd/sys/appleapiopts.h:
+
+/Users/lee/Desktop/Computer_Systems/Macos/PureDarwin/src/Kernel/xnu/osfmk/i386/acpi.h:
